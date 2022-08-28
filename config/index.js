7e74530cfdda1262de68cfbdb037e3a1764fe590
XIaolenga <112293211+XIaolenga@users.noreply.github.com>
2022-08-28 10:09:41 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -25,14 +25,14 @@ export const config = {
     //marryDate: "2020-01-04"
     }
 
- {{date.DATA}}  
- 城市：{{city.DATA}}  
- 天气：{{weather.DATA}}  
- 最低气温: {{min_temperature.DATA}}  
- 最高气温: {{max_temperature.DATA}}  
- 今天是我们恋爱的第{{love_day.DATA}}天
- 今天是我们结婚的第{{marry_day.DATA}}天
- {{birthday_message.DATA}}
+// {{date.DATA}}  
+// 城市：{{city.DATA}}  
+// 天气：{{weather.DATA}}  
+// 最低气温: {{min_temperature.DATA}}  
+// 最高气温: {{max_temperature.DATA}}  
+// 今天是我们恋爱的第{{love_day.DATA}}天
+// 今天是我们结婚的第{{marry_day.DATA}}天
+// {{birthday_message.DATA}}
 
  {{note_en.DATA}}  
  {{note_ch.DATA}}
